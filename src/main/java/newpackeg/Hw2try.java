04a9588719d691380decdebb8ba60a8281470745
JillZB <jillbai2017@gmail.com>
2020-11-06 18:15:00 -0500

    Homework 7

---
+++ src/main/java/newpackeg/Hw2try.java
@@ -0,0 +1,8 @@
+package newpackeg;
+
+public class Hw2try {
+    public static void main(String[] args) {
+        System.out.println("New package==new file");
+        System.out.println("dont forget to comit and push it");
+    }
+}
