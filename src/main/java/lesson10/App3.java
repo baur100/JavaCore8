47d3188221827b81dcf0eb100578ff42bd75867b
baur <baurzhan.zh@gmail.com>
2020-11-10 23:08:47 -0500

    Lesson 10 - Classes Extended, Enums, toString

---
+++ src/main/java/lesson10/App3.java
@@ -0,0 +1,9 @@
+package lesson10;
+
+public class App3 {
+    public static void main(String[] args) {
+        Subjects[] olegSubjects = { Subjects.BIOLOGY,Subjects.HISTORY };
+        Student oleg = new Student("Oleg","Petrov",5555,olegSubjects);
+
+    }
+}
