e505d8d55291a56a3e2c6c98bb147d23d53f82e6
Roman Belov <belov.roman.a@gmail.com>
2020-11-12 21:58:19 -0500

    Student

--- src/main/java/lesson10/App.java
+++ src/main/java/lesson10/App.java
@@ -2,6 +2,12 @@ package lesson10;
 
 public class App {
     public static void main(String[] args) {
+        Subjects[] joeSubjects = {Subjects.BIOLOGY, Subjects.IT};
+
+        Student joe = new Student("Joe", "biden", joeSubjects);
+
+        System.out.println(joe);
+
 
     }
 }
