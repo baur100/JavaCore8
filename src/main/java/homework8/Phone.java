7b6ec0433d667fa4fc3eae206c9a4c6758156bf5
Stan <stan.boretsky@gmail.com>
2020-11-14 12:19:21 -0500

    Homework8 - My homework

---
+++ src/main/java/homework8/Phone.java
@@ -0,0 +1,10 @@
+package homework8;
+
+public class Phone {
+    public String brand;
+    public String model;
+    public int memoryStorage;
+    public void getPhoneInfo(){
+        System.out.println("Phone brand: "+brand+". Model: "+model+" . Memory: "+memoryStorage+".");
+    }
+}
