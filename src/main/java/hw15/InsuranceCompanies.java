ec19fc6e1f6367c00fc29de7f7fbc4a4a7e6ce37
Michael Krasickiy <krasickiymichael@gmail.com>
2020-11-20 22:06:57 +0300

    hw14

---
+++ src/main/java/hw15/InsuranceCompanies.java
@@ -0,0 +1,10 @@
+package hw15;
+
+public enum InsuranceCompanies {
+    ALPHA_INSURANCE,
+    INGOSSTRAKH,
+    SOGAS,
+    RENAISSANCE_INSURANCE,
+    INCOME_INSURANCE,
+
+}
---
+++ src/main/java/hw15/Position.java
@@ -0,0 +1,9 @@
+package hw15;
+
+public enum Position {
+    PHYSICIAN,
+    THERAPIST,
+    HEMATOLOGIST,
+    CARDIOLOGIST,
+    RHEUMATOLOGIST,
+}
