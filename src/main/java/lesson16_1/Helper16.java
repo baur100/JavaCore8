86d42b7da7dcfe70ee41a6b27fae970f8470078a
Michael Krasickiy <krasickiymichael@gmail.com>
2020-11-22 00:54:25 +0300

    l16

--- src/main/java/lesson16_1/Helper16.java
+++ src/main/java/lesson16_1/Helper16.java
@@ -4,6 +4,6 @@ public class Helper16 {
     private String hi = "Hi";
     public static void printCircleLength(double radius){
         System.out.println(Math.PI*2*radius);
-        System.out.println(hi);
+ //       System.out.println(hi);
     }
 }
