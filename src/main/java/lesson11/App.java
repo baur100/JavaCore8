094eee8814cc5775a32fc003bac1bb2ab8dd2a0e
Roman Belov <belov.roman.a@gmail.com>
2020-11-21 17:35:15 -0500

    Update after merged earlier

--- src/main/java/lesson11/App.java
+++ src/main/java/lesson11/App.java
@@ -1,15 +1,15 @@
-package lesson11;
-
-public class App {
-    private final static int XX = 5;
-    public static void main(String[] args) {
-        Address davidAddress = new Address("125 Elm st", "Gotham","IL",50601);
-        Student david = new Student("David","Ross",1985,davidAddress,Subjects.BIOLOGY,"MIT", (short) 5);
-
-        Address janeAddress = new Address("135 Elm st", "Gotham","IL",50601);
-        Teacher jane = new Teacher("Jane","Rakowski",1989,janeAddress,Subjects.CHEMISTRY);
-
-        System.out.println(jane.getAddress().getStreetAddress1());
-
-    }
-}
+//package lesson11;
+//
+//public class App {
+//    private final static int XX = 5;
+//    public static void main(String[] args) {
+//        Address davidAddress = new Address("125 Elm st", "Gotham","IL",50601);
+//        Student david = new Student("David","Ross",1985,davidAddress,Subjects.BIOLOGY,"MIT", (short) 5);
+//
+//        Address janeAddress = new Address("135 Elm st", "Gotham","IL",50601);
+//        Teacher jane = new Teacher("Jane","Rakowski",1989,janeAddress,Subjects.CHEMISTRY);
+//
+//        System.out.println(jane.getAddress().getStreetAddress1());
+//
+//    }
+//}
