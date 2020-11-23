98d92f9ef8565597fa6e7a7b8478f1ce2a347cdc
Hanna <hannatrus01@gmail.com>
2020-11-23 23:25:48 +0000

    homework15.1

---
+++ src/main/java/hm15/InsuranseCompanies.java
@@ -0,0 +1,9 @@
+package hm15;
+
+public enum InsuranseCompanies {
+    UHC,
+    MEDICARE,
+    AETNA,
+    BCBS,
+
+}
---
+++ src/main/java/hm15/Position.java
@@ -0,0 +1,10 @@
+package hm15;
+
+public enum Position {
+    PEDIATRICIAN,
+    CARDIOLOGYST,
+    DERMATOLOGIST,
+    PHYSICIAN,
+    SURGEON,
+
+}
