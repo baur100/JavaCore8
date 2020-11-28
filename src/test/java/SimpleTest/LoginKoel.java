4f05f3d76c38650db092d7cb6cf7e288685d1e17
baur <baurzhan.zh@gmail.com>
2020-11-28 17:23:06 -0500

    Lesson 5 - BeforeMethod - AfterMethod

--- src/test/java/SimpleTest/LoginKoel.java
+++ src/test/java/SimpleTest/LoginKoel.java
@@ -1,4 +1,4 @@
-package simpleTest;
+package SimpleTest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -6,22 +6,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class KoelLogin {
+import java.util.List;
 
-// [type='email']
-// [type='password']
-// button
-// .home
-    @Test
-    public void loginToApp() throws InterruptedException {
-        // Arrange
+public class LoginKoel {
+    private WebDriver driver;
+    @BeforeMethod
+    public void startUp() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://koelapp.testpro.io/");
         Thread.sleep(2000);
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
 
+    @Test
+    public void loginToApp() throws InterruptedException {
         // Act
         WebElement email = driver.findElement(By.cssSelector("[type='email']"));
         WebElement password = driver.findElement(By.cssSelector("[type='password']"));
@@ -39,17 +46,9 @@ public class KoelLogin {
         } catch (NoSuchElementException xx){
             Assert.assertTrue(false,"Home element not found");
         }
-
-        Thread.sleep(3000);
-        driver.quit();
     }
     @Test
     public void wrongLoginToApp() throws InterruptedException {
-        // Arrange
-        System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://koelapp.testpro.io/");
-        Thread.sleep(2000);
 
         // Act
         WebElement email = driver.findElement(By.cssSelector("[type='email']"));
@@ -61,18 +60,10 @@ public class KoelLogin {
         loginButton.click();
         Thread.sleep(1000);
         // Assert
-        try{
-            driver.findElement(By.cssSelector(".error"));
-            Assert.assertTrue(true);
-        } catch (NoSuchElementException xx){
-            Assert.assertTrue(false,"Error element not found");
-        }
 
-        Thread.sleep(3000);
-        driver.quit();
+        List<WebElement> list = driver.findElements(By.cssSelector(".error"));
+        Assert.assertEquals(list.size(),1);
     }
-    @Test
-    public void udemy(){
         // //*[@alt='Headshot Image' and @src='https://s.udemycdn.com/topic-images/lohp-topic-banners/GettyImages-1166389425_opt.jpg']"
         // //[contains(@src,'1166389425')]
         // //*[text()='Learn. Grow. Succeed.']
@@ -82,8 +73,4 @@ public class KoelLogin {
         // /parent::*
         // (//nav)/following-sibling::*[5]
         //*[@class='header--gap-auth-button--7KoL0'])[1]/preceding-sibling::*[5]
-
-
-
-    }
 }
