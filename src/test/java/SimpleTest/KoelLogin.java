b4207be2ad29fed36f8d41ac3fffdb6655048459
nigoraA <nigoraastanova@gmail.com>
2020-11-29 20:43:35 -0500

    WebDriver

--- src/test/java/SimpleTest/KoelLogin.java
+++ src/test/java/SimpleTest/KoelLogin.java
@@ -1,19 +1,27 @@
 package SimpleTest;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class KoelLogin {
+    private WebDriver driver;
 
 //[type='email']
-////[type='passwrd']
+////[type='password']
 ////button
 //.home
 
-    public class KoelL0gin {
-        @Test
-        public void loginToApp() {
-            System.setProperties("webdriver.chrome.driver", "chromedriver.exe");
-            WebDriver driver = new CromeDriver();
-        }
+    @Test
+    public void loginToApp() {
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://koelapp.testpro.io/");
+
+
+        driver.close();
+
     }
 }
+
