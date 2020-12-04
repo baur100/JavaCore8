87a564004f5c1a19d84602e3b02d66ef1aa66a0e
baur <baurzhan.zh@gmail.com>
2020-12-03 22:54:06 -0500

    Lesson 7 - Pageobjects -1

---
+++ src/test/java/pageObjects/MainPage.java
@@ -0,0 +1,24 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MainPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(this.driver,10,200);
+    }
+
+    public boolean isMainPage(){
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".home")));
+            return true;
+        } catch (Exception xx){
+            return false;
+        }
+    }
+}
