bc9da72d844cb18c13700b1c61a4e7e647e67de3
natdik <natdik2017@yandex.com>
2020-12-04 09:48:53 -0500

    MyTest

---
+++ src/test/java/MyTest/LoginKoel.java
@@ -0,0 +1,55 @@
+package MyTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+// [type='email']
+// [type='password']
+// button
+// .home
+
+public class LoginKoel {
+    @Test
+    public void loginToApp() throws InterruptedException{
+//        Arrange
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://koelapp.testpro.io/");
+        Thread.sleep(2000);
+//        Act
+        WebElement email = driver.findElement(By.cssSelector("[type='email']"));
+        WebElement password = driver.findElement(By.cssSelector("[type='password']"));
+        WebElement  loginButton = driver.findElement(By.cssSelector("button"));
+
+        email.sendKeys("koeluser06@testpro.io");
+        password.sendKeys("te$t$tudent");
+        loginButton.click();
+        Thread.sleep(2000);
+
+//        Assert
+        try{
+            WebElement homeIcon = driver.findElement(By.cssSelector(".home"));
+            Assert.assertTrue(true);
+        } catch (NoSuchElementException xx){
+            Assert.assertTrue(false, "Home element not found");
+        }
+
+
+
+
+
+        Thread.sleep(3000);
+        driver.quit();
+
+
+
+        driver.close();
+
+    }
+}
