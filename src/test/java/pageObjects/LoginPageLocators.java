6f8ea12225e178b89e97760870179a8eda47a692
baur <baurzhan.zh@gmail.com>
2020-12-05 17:37:15 -0500

    Lesson 8-9 - Pageobjects-2, CreatePlylist, Fake, BasePage, BaseTest

---
+++ src/test/java/pageObjects/LoginPageLocators.java
@@ -0,0 +1,7 @@
+package pageObjects;
+
+public class LoginPageLocators {
+    public static final String emailCssSelector = "[type='email']";
+    public static final String passwordCssSelector = "[type='password']";
+    public static final String loginButtonCssSelector = "button";
+}
