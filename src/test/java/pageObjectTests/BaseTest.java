0a0452b10a5c981f4ea898ea0413d42559d7b6ae
baur <baurzhan.zh@gmail.com>
2020-12-12 17:20:21 -0500

    Lesson 12-13 Retry, Parameters, DataProvider, Logging

--- src/test/java/pageObjectTests/BaseTest.java
+++ src/test/java/pageObjectTests/BaseTest.java
@@ -5,11 +5,19 @@ import helper.BrowserFabric;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
+    protected String userName;
+    protected String password;
+
+    @Parameters({"username", "password"})
     @BeforeMethod
-    public void startUp() {
+    public void startUp(String email,String pwd) {
+        userName = email;
+        password = pwd;
         driver = BrowserFabric.getDriver(BrowserType.FIREFOX);
     }
     @AfterMethod
