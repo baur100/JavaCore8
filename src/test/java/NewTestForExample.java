5ffc3c78c726dc9e245e50df643f78cb65fe38c3
baur <baurzhan.zh@gmail.com>
2020-12-17 22:10:40 -0500

    QA-7874 - Add a unit tests

---
+++ src/test/java/NewTestForExample.java
@@ -0,0 +1,27 @@
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tickets.CalculateTicket;
+
+public class NewTestForExample {
+    @DataProvider(name = "Ages")
+    public Object[][] getData(){
+        return new Object[][]{
+                {1,0.0},
+                {6,500.0},
+                {25,1000},
+                {69,800.0}
+        };
+    }
+
+
+    @Test(dataProvider = "Ages")
+    public void testTicket(int age, double expectedPrice){
+
+        CalculateTicket calculator = new CalculateTicket(1000);
+
+        double ticketPrice = calculator.getPrice(age);
+
+        Assert.assertEquals(ticketPrice,expectedPrice);
+    }
+}
