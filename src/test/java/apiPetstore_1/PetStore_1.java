269ef3fb57813ce475c14dda538bd17b690042a8
Kras39 <butuzz32@gmail.com>
2020-12-30 06:53:43 +0300

    Merge remote-tracking branch 'origin/main' into KM40
    # Conflicts:
    #	JavaCore8.iml
    #	src/test/java/apiPetstore/PetStore.java

--- src/test/java/apiPetstore_1/PetStore_1.java
+++ src/test/java/apiPetstore_1/PetStore_1.java
@@ -1,4 +1,4 @@
-package apiPetstore;
+package apiPetstore_1;
 
 import io.restassured.path.json.JsonPath;
 import org.openqa.selenium.remote.Response;
@@ -6,10 +6,10 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
-public class PetStore {
+public class PetStore_1 {
     @Test
     public void getPetById_PetReturned(){
-        String baseUrl = "https://petstore.swagger.io/v2";
+        String baseUrl = "http://petstore.swagger.io/v2";
         String endPoint = "/pet/555";
 
         Response response = (Response) given()
@@ -23,6 +23,5 @@ public class PetStore {
                                 .response();
         JsonPath jsonPath = response.jsonPath();
         System.out.println(jsonPath.prettify());
-
+        }
     }
-}
