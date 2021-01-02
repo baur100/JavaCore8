1cce0b97a9707ea2c71fb70d52e211ee85ca8bde
Michael Krasickiy <krasickiymichael@gmail.com>
2021-01-03 02:16:54 +0300

    jc8

--- src/main/java/lesson9/App.java
+++ src/main/java/lesson9/App.java
@@ -1,8 +1,8 @@
-package lesson9;
+package Lesson9;
 
 public class App {
     public static void main(String[] args) {
-        Lesson9.Person Vasile = new Lesson9.Person("Vasile", "Ivan","10");
+        Person Vasile = new Person("Vasile", "Ivan","10");
         Vasile.setName("Vasile");
         Vasile.setLastname("Ivan");
         Vasile.setAge(10);
