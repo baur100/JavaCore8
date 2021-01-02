adad0ccebdb5a524e9bc1b007921970729899f03
Michael Krasickiy <krasickiymichael@gmail.com>
2021-01-03 02:17:12 +0300

    jc8

---
+++ src/test/java/apiPetstore_1/PetStore_1.java
@@ -0,0 +1,27 @@
+package apiPetstore_1;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PetStore_1 {
+    @Test
+    public void getPetById_PetReturned(){
+        String baseUrl = "https://petstore.swagger.io/v2";
+        String endPoint = "/pet/555";
+
+        Response response = given()
+                .baseUri(baseUrl)
+                .basePath(endPoint)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println(jsonPath.prettify());
+    }
+}
