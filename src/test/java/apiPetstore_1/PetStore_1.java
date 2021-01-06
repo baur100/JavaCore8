596d172b52a4c6dcd8cc2083e17c1543b9a84a5f
Michael Krasickiy <krasickiymichael@gmail.com>
2021-01-06 06:10:45 +0300

    jc8

--- src/test/java/apiPetstore_1/PetStore_1.java
+++ src/test/java/apiPetstore_1/PetStore_1.java
@@ -1,20 +1,43 @@
 package apiPetstore_1;
 
+import helper_1.TestDataGenerator_1;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-<<<<<<< HEAD
-=======
+import models_1.Pet_1;
 import org.testng.Assert;
->>>>>>> origin/KM40
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class PetStore_1 {
+    private long id;
+    private Pet_1 pet;
+    @BeforeMethod
+    public void startUp(){
+        String baseUrl = "https://petstore.swagger.io/v2";
+        String endPoint = "/pet";
+        pet = TestDataGenerator_1.getPet();
+
+        Response response = given()
+                .baseUri(baseUrl)
+                .basePath(endPoint)
+                .header("Content-type","application/json")
+                .body(pet)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        JsonPath jsonPath = response.jsonPath();
+        id = jsonPath.getLong("id");
+        System.out.println(id);
+    }
     @Test
     public void getPetById_PetReturned(){
         String baseUrl = "https://petstore.swagger.io/v2";
-        String endPoint = "/pet/555";
+        String endPoint = "/pet/"+id;
 
         Response response = given()
                 .baseUri(baseUrl)
@@ -26,6 +49,14 @@ public class PetStore_1 {
                 .extract()
                 .response();
         JsonPath jsonPath = response.jsonPath();
-        System.out.println(jsonPath.prettify());
+        Pet_1 petResponse = jsonPath.getObject("$",Pet_1.class);
+
+        Assert.assertEquals(petResponse.getStatus(),pet.getStatus());
+        Assert.assertEquals(petResponse.getCategory().getName(),pet.getCategory().getName());
+        Assert.assertEquals(petResponse.getCategory().getId(),pet.getCategory().getId());
+        Assert.assertEquals(petResponse.getName(),pet.getName());
+        var xx= petResponse.getPhotoUrl().length;
+        System.out.println(petResponse.getPhotoUrl().length);
+//        Assert.assertEquals(petResponse.getPhotoUrl()[0],pet.getPhotoUrl()[0]);
     }
 }
--- src/test/java/helper_1/TestDataGenerator_1.java
+++ src/test/java/helper_1/TestDataGenerator_1.java
@@ -1,6 +1,13 @@
 package helper_1;
 
+import com.github.javafaker.Faker;
+import models_1.Category_1;
+import models_1.Pet_1;
+import models_1.Status_1;
+import models_1.Tag_1;
 import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
 
 public class TestDataGenerator_1 {
     public static String randomString(int length){
@@ -8,4 +15,15 @@ public class TestDataGenerator_1 {
         boolean useNumbers = false;
         return RandomStringUtils.random(length, useLetters, useNumbers);
     }
+    public static Pet_1 getPet(){
+        Random random = new Random();
+        Faker faker = new Faker();
+
+        Category_1 category_1 = new Category_1(random.nextInt(20),faker.dog().breed());
+        Tag_1[] tags_1 = {new Tag_1(random.nextInt(10),faker.cat().breed())};
+        String[] photoUrls = new String[1];
+        photoUrls[0]=faker.internet().url();
+        return new Pet_1(category_1,faker.funnyName().name(),photoUrls,tags_1, Status_1.sold);
+    }
 }
+
