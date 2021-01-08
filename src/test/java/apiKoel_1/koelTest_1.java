66c490104f21e4d9d31a28c7348244ef4e72e5f4
Kras39 <butuzz32@gmail.com>
2021-01-08 06:47:45 +0300

    API_Koel

--- src/test/java/apiKoel_1/koelTest_1.java
+++ src/test/java/apiKoel_1/koelTest_1.java
@@ -1,5 +1,27 @@
 package apiKoel_1;
 
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import models.Pet;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class koelTest_1 {
-    
+    @Test
+    public void getPetById_PetReturned(){
+        String baseUrl = "https://koelapp.testpro.io/";
+        String endPoint = "api/me;
+
+        Response response = given()
+                .baseUri(baseUrl)
+                .basePath(endPoint)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        JsonPath jsonPath = response.jsonPath();
+        Pet petResponse = jsonPath.getObject("$",Pet.class);
 }
