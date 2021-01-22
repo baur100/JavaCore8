388dbbd3a8c9b3e390b49fa478c0bb1b83a3831b
Michael Krasickiy <krasickiymichael@gmail.com>
2021-01-22 06:30:57 +0300

    apiKoelTest

--- src/test/java/hybridTest_1/RenamePlaylist_1.java
+++ src/test/java/hybridTest_1/RenamePlaylist_1.java
@@ -1,32 +1,39 @@
 package hybridTest_1;
 
 import com.github.javafaker.Faker;
+import enums.BrowserType;
+import helper.BrowserFabric;
 import helper.Token;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import models.CreatePlaylistRequest;
 import models.CreatePlaylistResponse;
+import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pageObjects.LoginPage;
 
 import static io.restassured.RestAssured.given;
 
 public class RenamePlaylist_1 {
-    @Test
-    private int playlistId;
+    private String playlistId;
     private String token;
     private String baseUrl;
     Faker faker;
+    WebDriver driver;
+
     @BeforeClass
-    private void runOnce(){
+    private void runOnce() {
         faker = new Faker();
         baseUrl = "https://koelapp.testpro.io/";
         token = Token.get();
     }
+
     @BeforeMethod
-    public void startUp(){
+    public void startUp() {
         String endPoint = "api/playlist";
         String playlistName = faker.artist().name();
         System.out.println(playlistName);
@@ -35,7 +42,7 @@ public class RenamePlaylist_1 {
                 .baseUri(baseUrl)
                 .basePath(endPoint)
                 .header("Content-type", "application/json")
-                .header("Authorization",token)
+                .header("Authorization", token)
                 .body(createPlaylistRequest)
                 .when()
                 .post()
@@ -45,7 +52,28 @@ public class RenamePlaylist_1 {
                 .response();
         JsonPath jsonPath = response.jsonPath();
         CreatePlaylistResponse createPlaylistResponse = jsonPath.getObject("$", CreatePlaylistResponse.class);
-        playlistId = createPlaylistResponse.getId();
-        Assert.assertEquals(playlistName,createPlaylistResponse.getName());
+        playlistId = createPlaylistResponse.getId()+"";
+        driver = BrowserFabric.getDriver(BrowserType.CHROME);
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+        String endPoint = "api/playlist/"+playlistId;
+        given()
+                .baseUri(baseUrl)
+                .basePath(endPoint)
+                .header("Authorization",token)
+                .when()
+                .delete();
+    }
+    @Test
+    public void renamePlaylist_playlistRenamed(){
+        var loginPage = new LoginPage(driver);
+        loginPage.open();
+        var mainPage = loginPage.login("koeluser06@testpro.io","te$t$tudent");
+        var name = faker.animal().name();
+        System.out.println(name);
+        mainPage.renamePlaylist(playlistId,name);
+        Assert.assertTrue(mainPage.isPlaylistExist(playlistId,name));
     }
 }
